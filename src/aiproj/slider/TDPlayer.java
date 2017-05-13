6c3c8816150afc766f53103ed33340c809525420
David Stern <dibstern@gmail.com>
2017-05-13 17:55:56 +1000

    TDLeaf player added; implemented incorrectly. For review & correction or redesign.

--- src/aiproj/slider/TDPlayer.java
+++ src/aiproj/slider/TDPlayer.java
@@ -2,14 +2,17 @@ package aiproj.slider;
 
 import java.lang.Character;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static aiproj.slider.Input.*;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.Math.tanh;
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
 
 
-public class DopePlayer implements SliderPlayer {
+public class TDPlayer implements SliderPlayer {
 
     // Keeping Track of the Board, Player Piece Types, Number of current legal moves
     private Board curr_board;
@@ -19,12 +22,26 @@ public class DopePlayer implements SliderPlayer {
     private ArrayList<Tile> playerTiles;
     private ArrayList<Tile> opponentTiles;
 
+    //private ArrayList<Double> weights = new ArrayList<Double>(Arrays.asList(-0.9, -1.0, 1.0, 0.5, -0.5));
+    private ArrayList<Double> weights = new ArrayList<Double>(Arrays.asList(-0.9, 1.0, 1.0));//0.5));
+    //private ArrayList<Double> weights = new ArrayList<Double>(Arrays.asList(-0.7310103631330621, 1.6541847814522996, -1.4328774941556695));
+    //private ArrayList<Double> weights = new ArrayList<Double>(Arrays.asList(-0.99, 1.83460638307959, 0.4252092801559895));
+    private ArrayList<Board> positions = new ArrayList<Board>();
+
     // debug
     private static final Boolean debug = false;
     ArrayList<ArrayList<ArrayList<Double>>> vals;
     // end debug
 
     private static final int MAX_DEPTH = 7;
+    private static final double EPS = 0.05;
+    private static final double ALPHA = 1.0;
+    private static final int MAX_ITERATIONS = 200;
+    private static final double SHRINK_FACTOR = 1;  // 0.01;
+
+    // Learn Weights w/ TDLeaf?
+    private static final Boolean td = true;
+    private int movecount = 0;
 
     // Add more info to track here, for the Evaluation function
 
@@ -40,6 +57,7 @@ public class DopePlayer implements SliderPlayer {
      */
     public void init(int dimension, String board, char player) {
 
+        //String fileName = "weights.txt";
 
         // Assigning Player Pieces
         String playerType = Character.toString(player);
@@ -54,6 +72,7 @@ public class DopePlayer implements SliderPlayer {
 
         // Getting All Possible Moves
         refresh(curr_board);
+        saveBoard(curr_board);
     }
 
 
@@ -68,9 +87,37 @@ public class DopePlayer implements SliderPlayer {
      * move at the beginning of the game, move = null.
      */
     public void update(Move move) {
-
         modifyBoard(curr_board,move);
         refresh(curr_board);
+    }
+
+    private void saveBoard(Board board){
+        positions.add(board);
+    }
+
+    public void tdLeaf(ArrayList<Board> positions, double leafUtility) {
+        for (int i = movecount; i < positions.size(); i++) {
+            updateWeights(positions.get(i), leafUtility, i);
+        }
+        System.out.println("Weights: " + weights);
+    }
+
+    private void updateWeights(Board position, double trueUtil, int j) {
+        double util = evaluateBoard(position);
+        ArrayList<Double> board_features = evalFeatures(position);
+        int iterations = MAX_ITERATIONS;
+        while (abs(util - trueUtil) > EPS && iterations > 0) {
+            for (int i = 0; i < weights.size(); i++) {
+
+                //double new_weight = weights.get(i) - ALPHA*(pow(util - trueUtil, j))*board_features.get(i);
+                //double new_weight = weights.get(i) - j*ALPHA*(util - trueUtil)*board_features.get(i);
+                double new_weight = weights.get(i) - ALPHA*(util - trueUtil)*board_features.get(i);
+                weights.set(i, new_weight);
+                util = evaluateBoard(position);
+            }
+            //System.out.println("Iteration: " + (iterations));
+            iterations -= 1;
+        }
     }
 
 
@@ -159,8 +206,8 @@ public class DopePlayer implements SliderPlayer {
         return firstMove;
     }
 
-    private double evaluateBoard(Board board) {
 
+    private ArrayList<Double> evalFeatures(Board board) {
         // Set list of tiles based on who the player is
         if (ourPlayer.equals(Tile.PLAYER_H)) {
             this.playerTiles = board.getHTiles();
@@ -175,19 +222,39 @@ public class DopePlayer implements SliderPlayer {
             System.out.println("Catastrophic error: incorrect player name");
             System.exit(0);
         }
-
-
-        //FEATURE DEFINITION
-
-
-        // playerTileDifference is preferable to having a separate number for each player tiles
-        // This is because the tile difference will perform better in machine learning (fewer, less
-        // noisy possible values)
         double playerTileDifference = playerTiles.size() - opponentTiles.size();
 
-        // Sum the distances of each player
-        double sumPlayerDistances = sumDistances(playerTiles,  board.getLength());
-        double sumOpponentDistances = sumDistances(opponentTiles, board.getLength());
+        // Need our features to be of a relatively consistent size across the game, so as to not distort the weights
+        // Maximise the opponent's distances, minimise our own distances; positive weight
+        double dist_advantage = sumDistances(opponentTiles, boardsize) - sumDistances(playerTiles,  boardsize);
+        //double sumPlayerDistances = sumDistances(playerTiles,  board.getLength());
+        //double sumOpponentDistances = sumDistances(opponentTiles, board.getLength());
+
+        // Added Forward Moves -> Players were starting out by choosing the first possible move (moves.get(0))
+        // Maximise possible forward moves for player, minimise for opponent
+
+        double forwardMovesOpponent = forwardMoves(board.getMovesOpponent(), Opponent);
+        double forwardMovesPlayer = forwardMoves(board.getMovesPlayer(), ourPlayer);
+
+        // Maximise our forward moves in relation to number of pieces, minimise Opponent forward moves irt num pieces
+        // Positive Weight (If we have more forward moves and this is positive, that's good)
+        double forwardAdv = (forwardMovesPlayer / playerTiles.size()) - (forwardMovesOpponent / opponentTiles.size());
+        forwardAdv *= boardsize - 1;
+        // Add all features to an ArrayList
+        ArrayList<Double> features = new ArrayList<Double>();
+        features.add(playerTileDifference);       // -0.9
+        //features.add(sumPlayerDistances);                      // -1.0
+        //features.add(sumOpponentDistances);                    // 1.0
+        features.add(dist_advantage);             // 1.0 +
+        features.add(forwardAdv);                 // 0.5 +
+        //features.add(forwardMovesPlayer);                     // 0.5
+        //features.add(forwardMovesOpponent);                     // -0.5
+        return features;
+    }
+
+
+    private double evaluateBoard(Board board) {
+        ArrayList<Double> features = evalFeatures(board);
 
         // The end state of the game should be properly evaluated by the evaluation function.
         if (opponentTiles.size() == 0) {
@@ -197,45 +264,24 @@ public class DopePlayer implements SliderPlayer {
             return 1;
         }
 
-        // Maximise possible forward moves for player, minimise for opponent
-        double forwardMovesOpponent = forwardMoves(board.getMovesOpponent(), Opponent);
-        double forwardMovesPlayer = forwardMoves(board.getMovesPlayer(), ourPlayer);
-
-        // Add all features to an arraylist and define weights
-        // Weights are presently +ve (good) or -ve (bad)
-        // Eventually we will define these elsewhere with Machine Learning
-
-        ArrayList<Double> features = new ArrayList<Double>();
-        ArrayList<Double> featureWeights = new ArrayList<Double>();
-        features.add(playerTileDifference);
-        featureWeights.add(-0.9);
-        features.add(sumPlayerDistances);
-        featureWeights.add(-1.0);
-        features.add(sumOpponentDistances);
-        featureWeights.add(1.0);
-
-        // Added Forward Moves -> Players were starting out by choosing the first possible move (moves.get(0))
-        features.add(forwardMovesPlayer);
-        featureWeights.add(0.5);
-        features.add(forwardMovesOpponent);
-        featureWeights.add(-0.5);
-
         // Sum total based on feature weights
-
         double total = 0;
-        for (int i=0; i<featureWeights.size();i++) {
-            total += featureWeights.get(i) * features.get(i);
+        for (int i=0; i<weights.size();i++) {
+            //System.out.println("Feature: " + i + " == " + features.get(i) + "; Weight == " + weights.get(i));
+            total += weights.get(i) * features.get(i);
         }
-        // Squashes Evaluation function to between -1 and 1, and the .1 allows for the high feature weights.
-        return tanh(total*0.1);
+        // Squashes Evaluation function to between -1 and 1, and the shrink factor allows for the high feature weights.
+        return tanh(total*SHRINK_FACTOR);
     }
 
 
     private double forwardMoves(ArrayList<Move> moves, String player) {
+        double total = 0;
+
         if (moves.size() <1) {
             return 0;
         }
-        double total = 0;
+
         for (Move move : moves) {
             if (player.equals(Tile.PLAYER_H)) {
                 total += (move.d == Move.Direction.RIGHT) ? 1 : 0;
@@ -272,6 +318,40 @@ public class DopePlayer implements SliderPlayer {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // For each minimax Decision
+    // Create a list of boards representing the principle variation (the sequence of moves taken from the root to the
+    // optimal leaf, whose value is propagated backwards up to minimax), then perform tdLeaf on that list of board
+    // positions
+    // No, look at https://www.cs.princeton.edu/courses/archive/fall06/cos402/papers/chess-RL.pdf
+    // for each position t, except the last, compute the temporal difference (eval of leaf at t+1 - eval of leaf at t)
+    // and calculate the partial derivative of the eval(from t to depth d (minimax)). The weight update is alpha * the sum
+    // of all of these partial derivatives, where each derivative is:
+    // multiplied by the sum, from position j(=t) to the last position, of (lambda^(j-t) * the matching temporal difference
+
+
+
+
+
+
+
+
+
+
+
+
     public Move minimaxDecision(Board board) {
 
         ArrayList<Move> moves = board.getMovesPlayer();
@@ -303,14 +383,27 @@ public class DopePlayer implements SliderPlayer {
             if (val > max) {
                 max = val;
                 maxMove = move;
+
             }
+            /*
+            ArrayList<Double> features = evalFeatures(newBoard);
+            for (int i=0; i<weights.size();i++) {
+                System.out.println("Feature: " + i + " == " + features.get(i) + "; Weight == " + weights.get(i));
+            }
+            */
         }
+        // With the 'True Utility' propagated from the leaf node, update our weights using tdLeaf
+        if (td) tdLeaf(positions, max);
         //debug
+
         if (debug) {
             vals.get(0).add(myVals);
-            System.out.println("DopePlayer " + ourPlayer + " max: " + max + " from the following minimax tree:");
+            System.out.println("TDPlayer " + ourPlayer + " max: " + max + " from the following minimax tree:");
+
             for (int i = 0; i < vals.size(); i++) {
-                System.out.println("depth " + i + " : " + vals.get(i));}}
+                System.out.println("depth " + i + " : " + vals.get(i));}
+
+        }
         // end debug
         return maxMove;
 
@@ -388,6 +481,17 @@ public class DopePlayer implements SliderPlayer {
 
         return value;
     }
+/*
+    private void updatePrevMinStates(Board newBoard, double value) {
+        if (!previous_min_states.containsKey(newBoard)) {
+            previous_min_states.put(newBoard, value);
+        }
+    }
+    private void updatePrevMaxStates(Board newBoard, double value) {
+        if (!previous_max_states.containsKey(newBoard)) {
+            previous_max_states.put(newBoard, value);
+        }
+    }*/
 
     private boolean terminalTest(int depth) {
         if (depth >= MAX_DEPTH) {
